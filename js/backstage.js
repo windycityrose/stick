b154b3e7b21849a2043167375dff7f6b8854b733
guagua20170515 <979992760@qq.com>
2017-08-04 14:38:31 +0800

    add backstage

--- js/backstage.js
+++ js/backstage.js
@@ -2,17 +2,47 @@
  * Created by 李华东 on 2017/8/4.
  */
 window.onload=function(){
-    var oDate=document.getElementsByClassName("date")[0];
+    var day_list=document.getElementsByClassName("date")[0];
     var aDate=new Date();
 
     var today=aDate.getDate();//今天
-
-    for(var i=0; i<10;i++){
-        aDate.setDate(today+i);
+    for(var i=0; i<15;i++){
+        aDate.setDate(today+1);
+        today=aDate.getDate();
         var sg=aDate.getMonth()+1;
-        var ss=aDate.getDate();
+        var ss=aDate.getDate()-1;
         var oli=document.createElement("li");
         oli.innerHTML=sg+"月"+ss+"日";
-        oDate.appendChild(oli);
+        day_list.appendChild(oli);
+    }
+    var ali=document.getElementsByClassName("morning")[0].getElementsByTagName("li");
+    for(var j=0;j<ali.length;j++){
+        ali[j].onclick=function(){
+            var img=this.getElementsByTagName("img")[0];
+            switch (img.style.display)
+            {
+                case "none":
+                    img.style.display="inline-block"
+                    break;
+                case "inline-block":
+                    img.style.display="none"
+                    break;
+            }
+        }
+    }
+    var ili=document.getElementsByClassName("after")[0].getElementsByTagName("li");
+    for(var k=0;k<ili.length;k++){
+        ili[k].onclick=function(){
+            var oimg=this.getElementsByTagName("img")[0];
+            switch (oimg.style.display)
+            {
+                case "none":
+                    oimg.style.display="inline-block"
+                    break;
+                case "inline-block":
+                    oimg.style.display="none"
+                    break;
+            }
+        }
     }
 };
