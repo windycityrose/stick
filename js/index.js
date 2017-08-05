63d1698d89dca9cac5139f400c70a1a6503cd4fc
guagua20170515 <979992760@qq.com>
2017-08-05 16:01:08 +0800

    add 2000

--- js/index.js
+++ js/index.js
@@ -170,7 +170,7 @@ window.onload = function () {
                 }
                 this.style.backgroundColor="#339EE2";
                 this.style.color="#fff";
-                document.body.scrollTop=3000;
+                document.body.scrollTop=2000;
 
                 alert(document.body.scrollTop);
                 console.log(oset);
