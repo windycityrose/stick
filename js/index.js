ba20ba1282d84d35ec35cd7136b0ff0ea6fd7a25
guagua20170515 <979992760@qq.com>
2017-08-07 11:11:49 +0800

    rrr

--- js/index.js
+++ js/index.js
@@ -178,7 +178,7 @@ window.onload = function () {
                     if (document.body.scrollTop >=otop) {
                         clearInterval(time);
                     }
-                }, 10);
+                }, 50);
                 table[this.index].style.display="block";
             }
         }
